a6918d4eda29ebecc0a8c1f0e5e1905a9b999b67
Oconnor Burton <oconnorburton387@gmail.com>
2025-01-09 17:27:10 -0500

    added class output

--- class.py
+++ class.py
@@ -1,27 +1,32 @@
 class Student:
     School = "Glenmuir"
 
-    def __init__(self):
-        self.id = 0
-        self.first_name = ""
-        self.last_name = ""
-
     def __init__(self, id, fisrt_name, last_name):
         self.id = id
         self.first_name = fisrt_name
         self.last_name = last_name
 
+
     def id_update(self, new_id):
         self.id = new_id
 
     def first_name_update(self, ln):
-        self.last_name = ln
+        self.first_name = ln
 
     def last_name_update(self, fn):
-        self.first_name = fn
+        self.last_name = fn
 
     def print_info(self):
         print(self.id)
         print(self.first_name + " " + self.last_name)
+        print(self.School)
+
+Oconnor = Student(2108222, "Oconnor", "Burton")
+Oconnor.print_info()
+
+Oconnor.first_name_update("Keno")
+Oconnor.print_info()
+
+
 
     
